a8a5c537349bae63246de71d89d17313401883be
Pedro F <34808198+pfuentes69@users.noreply.github.com>
2023-04-30 16:15:38 +0200

    Presence sensor added

--- papilloniot-mqttthing-wemos-am2320-bmp280/src/main.cpp
+++ papilloniot-mqttthing-wemos-am2320-bmp280/src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
-#include "BMP280.h"
 #include "Wire.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include "Adafruit_Sensor.h"
 #include "Adafruit_AM2320.h"
+#include <Adafruit_BMP280.h>
 
 #define P0 1013.25
 
@@ -23,10 +23,10 @@ WiFiClient WIFIclient;
 PubSubClient client(WIFIclient);
 
 // sleep for this many seconds
-const int sleepSeconds = 300;
+const int sleepSeconds = 600;
 
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
-BMP280 bmp;
+Adafruit_BMP280 bmp; // I2C
 
 void setup() {
   // Connect D0 to RST to wake up
@@ -36,6 +36,8 @@ void setup() {
   while (!Serial) {
     delay(10); // hang out until serial port opens
   }
+  Serial.flush();
+  Serial.println();
 
     // initialize WiFi
   WiFi.mode(WIFI_STA);
@@ -47,13 +49,13 @@ void setup() {
     WiFi.begin(ssid, password);
     tries = 0;    
     Serial.println("Trying Main WiFi");
-    while ((WiFi.status() != WL_CONNECTED) && (tries < 10)) {
-      delay(500);
+    while ((WiFi.status() != WL_CONNECTED) && (tries < 50)) {
+      delay(100);
       Serial.print(".");
       tries++;
     }
     Serial.println();
-    if (tries == 10) {
+    if (tries == 50) {
       Serial.println("Too many trials, we'll sleep and try later");
       ESP.deepSleep(sleepSeconds * 1000000);
     } else {
@@ -63,59 +65,67 @@ void setup() {
 
   Serial.println("");
   Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
+  Serial.print("Network: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println();
 
   //connect to MQTT server
   client.setServer(broker, 1883);
   client.connect("DHTSensor3");
 
   // Config sensors
-  am2320.begin();
-  bmp.begin();  
-  bmp.setOversampling(4);
+  if (!am2320.begin()) {
+    Serial.println(F("Could not find a valid AM2320 sensor, check wiring or "
+                      "try a different address!"));
+  }
 
+  if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+//  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+  } else {
+    // Default settings from datasheet.
+    bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     // Operating Mode. 
+                    Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling 
+                    Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling 
+                    Adafruit_BMP280::FILTER_X16,      // Filtering. 
+                    Adafruit_BMP280::STANDBY_MS_500); // Standby time. 
+
+  }
   // Run duty cycle
   double T,P,H;
-  char result = bmp.startMeasurment();
- 
-  if(result!=0){
-    delay(result);
-    result = bmp.getTemperatureAndPressure(T,P);
-    if(result!=0)
-      {
-        Serial.print("Pressure = \t");
-        Serial.print(P,2);
-        Serial.println(" mBar");
-        //        Serial.print("A = \t");Serial.print(A,2); Serial.println(" m");
-        Serial.print("Temperature = \t");
-        T = am2320.readTemperature();
-        Serial.print(T);
-        Serial.println(" C");
-        Serial.print("Humidity = \t");
-        H = am2320.readHumidity();
-        Serial.print(H);
-        Serial.println("%");
 
-        String payload = "{\"Temperature\":";
-        payload += T;
-        payload += ",\"Humidity\":";
-        payload += H;
-        payload += ",\"Pressure\":";
-        payload += P;
-        payload += "}";
+  // AM2320
+  T = am2320.readTemperature();
+  H = am2320.readHumidity();
+  Serial.printf("Temperature : %f\n", T);
+  Serial.printf("Humidity    : %f\n", H);
 
-        //Serial.println(payload);
-        client.publish("PapillonIoT/DHTSensor3/data", (char*) payload.c_str());
-        delay(500);
-      }
-      else {
-        Serial.println("Error.");
-      }
+  // BMP280
+  // must call this to wake sensor up and get new measurement data
+  // it blocks until measurement is complete
+  if (bmp.takeForcedMeasurement()) {
+    // can now print out the new measurements
+    P = bmp.readPressure();
+    Serial.printf("Pressure   : %f\n", P);
+  } else {
+    Serial.println("BMP Forced measurement failed!");
   }
-  else {
-    Serial.println("Error.");
-  }
+  Serial.println();
+
+  String payload = "{\"Temperature\":";
+  payload += T;
+  payload += ",\"Humidity\":";
+  payload += H;
+  payload += ",\"Pressure\":";
+  payload += P;
+  payload += "}";
+
+  //Serial.println(payload);
+  client.publish("PapillonIoT/DHTSensor3/data", (char*) payload.c_str());
+  delay(500);
 
   // Let's go to sleep
   ESP.deepSleep(sleepSeconds * 1000000);
